c22f7eeff7d9782bc938a60ba64992fab21ee5d2
Qice <1538933241@qq.com>
2018-05-28 10:32:32 +0800

    添加删除提示，将结算页面的分数保留两位小数

--- Assets/Scripts/UIScript/Start/ClickMovieListItem.cs
+++ Assets/Scripts/UIScript/Start/ClickMovieListItem.cs
@@ -7,9 +7,10 @@ using System.IO;
 public class ClickMovieListItem : MonoBehaviour
 {
     public GameObject PlayModeCanvas;
-
+    public GameObject WarningTextPrefab;
     private GameObject StartModeCanvas;
     private string strFilePath;
+   
 
     private void Start()
     {
@@ -24,19 +25,8 @@ public class ClickMovieListItem : MonoBehaviour
 
     public void OnClickDelet_Btn()
     {
-        var filepath = GetComponent<ClickMovieListItem>().GetFilePath();
-        ConfigCenter.Instance().DeleteFileByPath(GetComponent<ClickMovieListItem>().GetFilePath());
-        var temphead = FileReader.GetHeadFromFile(filepath);
-        var portraitpath = ToolFunction.GetDefaultPortraitPathByName(temphead.strPortrait, ".jpg");
-        if (File.Exists(portraitpath))
-        {
-            File.Delete(portraitpath);
-        }
-        if (File.Exists(filepath))
-        {
-            File.Delete(filepath);
-        }
-        Destroy(gameObject);
+        var WarningText = Instantiate(WarningTextPrefab, StartModeCanvas.transform);
+        WarningText.GetComponent<StartDeleteWarningText>().SetDeleteObj(gameObject);
     }
 
     public void SetFilePath(string astrFilePath)
